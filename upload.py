06db2e34302fe66330fa47c7c12b4e0276aa8e99
月 <wulan17@wulan17.my.id>
2023-04-07 22:52:50 +0700

    Update upload script and update kernel version to v6.2.10

--- upload.py
+++ upload.py
@@ -1,9 +1,13 @@
 #!/bin/python
 import asyncio
+import glob
 import os
+import re
 import sys
 
+from datetime import datetime
 from pyrogram import Client
+from pyrogram.types import InputMediaDocument
 
 BOT_SESSION = os.environ.get('TELEBOT', None)
 
@@ -13,9 +17,33 @@ client = Client(
 	sleep_threshold=180
 )
 
+all_files = glob.glob(f'{sys.argv[1]}/share/*.zst')
+
 async def upload():
 	client.loop = asyncio.get_event_loop()
 	await client.start()
-	await client.send_document(chat_id="@tsuki172", document=sys.argv[1])
+	media = []
+	count = len(all_files)
+	i = 1
+	version = None
+	caption = None
+	now = datetime.now().strftime('%d/%m/%Y')
+	r = r"(wulan17-)([0-9]{1,}\.[0-9]\.[0-9]{1,})(-[0-9]{1,}-x86_64)"
+	for file in all_files:
+		search = re.search(r, file)
+		if search:
+			version = search.group(2)
+		if i == count:
+			caption = f'''
+🆕 linux-xanmod-anbox v{version}
+👤 By wulan17
+🖥 Archlinux-based Distro
+⏱ Build Date {now}
+
+#archlinux #kernel #xanmod
+			'''
+		media.append(InputMediaDocument(media=file, caption=caption))
+		i = i+1
+	await client.send_media_group(chat_id="wulan17", media=media)
 
 asyncio.run(upload())
